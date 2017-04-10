1092ee0353031612717feaf8adfd5d63a3abab84
kevin.tinn <kevin.tinn@asynchrony.com>
2017-04-09 23:19:15 -0500

    runs as web service

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Self Staffing System
 A tool to provide suggestions on teams you would fit on and developers you are similar to skills-wise
+
+* To run, in R Studio:
+ ```
+library(plumber)
+app = plumb('main.r')
+app$run(port=8888)
+```
+
+
+* Example call: ```localhost:8888/suggest?html=3&js=4&net=5&java=0&functional=0=&ios=0&android=1```
+
--- main.r
+++ main.r
@@ -1,25 +1,21 @@
-#install.packages("class")
 #library(class)
+#library(plumber)
 
-set.seed(2)# i think i need this to reproduce outcome??
-
-raw = data.frame(read.csv('data/devData.csv', stringsAsFactors = F))
-data = raw[-1]#remove dev name
-data$Team  = factor(data$Team) # replace stringish team name with factor
-
-training = data[which(data$Team != ""), ]
-trainingData = training[, 1:7]
-trainingLabels = training[,8] # extract factor representing team name for rows of training data
-
-testData = data[which(data$Team == ""), 1: 7]
-
-# odd number close to sq root of number of rows, odd desired cause it doesnt lead to tie
-k = round(sqrt(nrow(trainingData))) #todo: add logic to round to odd when round would lead to even
-
-predictions = knn(train = trainingData, test = testData, k = k, cl = trainingLabels)
-
-#/ad hoc-ish analysis example
-testData2 = matrix(nrow = 1, c(1, 1, 0, 2, 2, 1, 5))#good at android
-testData2 = rbind(testData2, c(1, 1, 0, 2, 2, 4, 2))#ios
-
-predictions2 = knn(train = trainingData, test = testData2, k = k, cl = trainingLabels)
+#* @get /suggest
+suggestTeam = function(html,	js,	net, java,	functional, ios, android) {
+  set.seed(2)# i think i need this to reproduce outcome??
+  
+  raw = data.frame(read.csv('data/devData.csv', stringsAsFactors = F))
+  data = raw[-1]#remove dev name
+  data$Team  = factor(data$Team) # replace stringish team name with factor
+  
+  training = data[which(data$Team != ""), ]
+  trainingData = training[, 1:7]
+  trainingLabels = training[, 8] # extract factor representing team name for rows of training data
+  
+  # odd number close to sq root of number of rows, odd desired cause it doesnt lead to tie
+  k = round(sqrt(nrow(trainingData))) #todo: add logic to round to odd when round would lead to even  
+  
+  rating = matrix(nrow = 1, c(as.numeric(html), as.numeric(js), as.numeric(net), as.numeric(java), as.numeric(functional), as.numeric(ios), as.numeric(android)))
+  knn(train = trainingData, test = rating, k = k, cl = trainingLabels)
+}
