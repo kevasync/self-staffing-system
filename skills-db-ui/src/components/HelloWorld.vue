e580ec3f4302b751f183b0ac6c80989730ce5440
kevin.tinn <kevin.tinn@asynchrony.com>
2018-05-27 14:12:05 -0500

    Showing filterable/selectable list of skills

--- skills-db-ui/src/components/HelloWorld.vue
+++ skills-db-ui/src/components/HelloWorld.vue
@@ -1,94 +1,53 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <h1>Staff O' Matic</h1>
+    <h3>powered by vue.js</h3>
+    <div>
+      <div>
+        <h4>Pick a skill:</h4>
+          <ul>
+            <li v-for="c in filteredCategories" v-on:click="selectSkill(c)">{{ c }}</li>
+          </ul>
+      </div>
+      <div><input type="text" v-model="categoryFilter" @change="filterCategories"></div>
+      <div>
+        <h3>{{ selectedSkills.length }} skills selected:</h3>
+        <ul>
+            <li v-for="s in selectedSkills">{{ s }}</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+let categories = ['js', 'html', 'java']
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      counter: 0, 
+      filteredCategories: categories,
+      categoryFilter: '',
+      selectedSkills: []
+    }
+  },
+  methods: {
+    filterCategories: function (event) {
+      if(this.categoryFilter.length > 1) {
+        this.filteredCategories = categories.filter(c => c.toLowerCase().startsWith(this.categoryFilter.toLowerCase()))
+      }
+      else {
+       this.filteredCategories = categories 
+      } 
+    },
+    selectSkill: function(skill) {
+      let i = this.selectedSkills.indexOf(skill)
+      if(i == -1){
+        this.selectedSkills.push(skill)  
+      } else {
+        this.selectedSkills.splice(i, 1)
+      }
     }
   }
 }
